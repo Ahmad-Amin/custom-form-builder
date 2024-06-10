6b9d5081a098a444ee443ede034decbd8034de5f
Ahmad-Amin <baigahmad323@gmail.com>
2024-06-10 12:55:44 +0500

    Added Google analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Comfortaa } from "next/font/google";
 import { ConfigProvider } from "antd";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 const comfortaa = Comfortaa({ subsets: ["latin"] });
 
@@ -12,19 +13,35 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          fontFamily: comfortaa.style.fontFamily
-        }
-      }}
-    >
-      <html lang="en">
-        <body className={comfortaa.className}>
-          <Toaster />
-          {children}
-        </body>
-      </html>
-    </ConfigProvider>
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-WQTSYE4K0T"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-WQTSYE4K0T');
+        `}
+      </Script>
+      <ConfigProvider
+        theme={{
+          token: {
+            fontFamily: comfortaa.style.fontFamily
+          }
+        }}
+      >
+        <html lang="en">
+          <body className={comfortaa.className}>
+            <Toaster />
+            {children}
+          </body>
+        </html>
+      </ConfigProvider>
+    </>
   );
 }
